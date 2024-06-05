95dfb25d6708841092026aa72a1ae536d7b465e5
nickamano <nick.amano@yahoo.com>
2024-06-05 10:57:33 -0400

    add batch job

---
+++ jobs/LOM2SEM_Palette_all.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# The interpreter used to execute the script
+
+#“#SBATCH” directives that convey submission options:
+
+#SBATCH --job-name=LOM2SEM-pal-all
+#SBATCH --mail-user=namano@umich.edu
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --cpus-per-task=2
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem-per-cpu=8g
+#SBATCH --time=00:01:00:00
+#SBATCH --account=eaholm0
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:2
+#SBATCH --output=/home/%u/logs/LOM2SEM/palette/all/%x-%j.log
+
+# The application(s) to execute along with its input arguments and options:
+
+cd ~/research/LOM2SEM
+module load python/3.12.1
+source venv/L2S/bin/activate
+python -m Palette.run -p train -c Palette/config/gd_kaiming.json -d
---
+++ jobs/LOM2SEM_Palette_debug.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# The interpreter used to execute the script
+
+#“#SBATCH” directives that convey submission options:
+
+#SBATCH --job-name=LOM2SEM-pal-all
+#SBATCH --mail-user=namano@umich.edu
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --cpus-per-task=2
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem-per-cpu=8g
+#SBATCH --time=1:00:00:00
+#SBATCH --account=eaholm0
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:2
+#SBATCH --output=/home/%u/logs/LOM2SEM/palette/all/%x-%j.log
+
+# The application(s) to execute along with its input arguments and options:
+
+cd ~/research/LOM2SEM
+module load python/3.12.1
+source venv/L2S/bin/activate
+python -m Palette.run -p train -c Palette/config/gd_kaiming.json 
+python -m Palette.run -p train -c Palette/config/gd_normal.json 
+python -m Palette.run -p train -c Palette/config/gd_xavier.json 
+python -m Palette.run -p train -c Palette/config/sr3_kainming.json 
+python -m Palette.run -p train -c Palette/config/sr3_normal.json 
+python -m Palette.run -p train -c Palette/config/sr3_xavier.json 
