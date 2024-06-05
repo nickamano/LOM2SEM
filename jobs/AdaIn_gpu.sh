912283009cc3bfc4da7e727eb5eea2a494b0e8b1
Nick Amano <namano@gl-login3.arc-ts.umich.edu>
2024-06-05 09:59:09 -0400

    aDAIN config and jobs

--- jobs/AdaIn_gpu.sh
+++ jobs/AdaIn_gpu.sh
@@ -3,14 +3,14 @@
 
 #“#SBATCH” directives that convey submission options:
 
-#SBATCH --job-name=gl-LOM2SEM-adain-gpu
+#SBATCH --job-name=LOM2SEM-adain-gpu
 #SBATCH --mail-user=namano@umich.edu
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --cpus-per-task=2
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --mem-per-cpu=8g
-#SBATCH --time=02:00:00
+#SBATCH --time=04:00:00
 #SBATCH --account=eaholm0
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
@@ -19,5 +19,6 @@
 # The application(s) to execute along with its input arguments and options:
 
 cd ~/research/LOM2SEM
+module load python/3.12.1
 source venv/L2S/bin/activate
-python -m AdaIN.train_adain --config AdaIN/configs/AdaIn_gpu.yaml
+python -m AdaIN.train_adain --config AdaIN/configs/AdaIn_gpu-03.yaml
--- jobs/stylegan.sh
+++ jobs/stylegan.sh
@@ -6,22 +6,24 @@
 #SBATCH --job-name=LOM2SEM-style-gpu
 #SBATCH --mail-user=namano@umich.edu
 #SBATCH --mail-type=BEGIN,END,FAIL
-#SBATCH --cpus-per-task=3
+#SBATCH --cpus-per-task=2
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --mem-per-cpu=4g
 #SBATCH --time=01:00:00
 #SBATCH --account=eaholm0
 #SBATCH --partition=gpu
-#SBATCH --gres=gpu:2
+#SBATCH --gres=gpu:1
 #SBATCH --output=/home/%u/logs/LOM2SEM/stylegan/%x-%j.log
 
 # The application(s) to execute along with its input arguments and options:
 
 cd ~/research/LOM2SEM
-source venv/L2S/bin/activate
-module load cuda/12.3.0
+module load cuda/10.2.89
+module load python3.10-anaconda/2023.03
+eval "$(conda shell.bash hook)"
+conda activate L2Sstyle
 python ./stylegan2/train.py \
 	--outdir=/nfs/turbo/coe-eaholm/namano/LOM2SEM/results/stylegan2 \
 	--lomdata=/nfs/turbo/coe-eaholm/namano/LOM2SEM/mecs_steel/train_LOM512/ \
-       	--semdata=/nfs/turbo/coe-eaholm/namano/LOM2SEM/mecs_steel/train_SEM512/ \
+       	--semdata=/nfs/turbo/coe-eaholm/namano/LOM2SEM/mecs_steel/train_SEM512/ 
